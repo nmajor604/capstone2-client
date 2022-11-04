f4111c712e5298a2e5329c82386b8378775eadc6
nmajor604 <81140580+nmajor604@users.noreply.github.com>
2022-11-03 23:43:35 -0700

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import { Route, Switch, BrowserRouter as Router } from 'react-router-dom';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import Header from './Components/Header/Header';
+import Home from './Components/Home/Home';
+import Listings from './Components/Listings/Listings';
+import Login from './Components/Login/Login';
+import NewListing from './Components/NewListing/NewListing';
+import NewSeller from './Components/NewSeller/NewSeller';
+import SellerHome from './Components/SellerHome/SellerHome';
+import Footer from './Components/Footer/Footer';
+
+
+export default class App extends Component {
+
+   
+
+  render() {
+    return (
+          <Router>
+            <Header />
+              <Switch>
+                <Route exact path="/" component={Home} />
+                <Route exact path="/listings" component={Listings} />
+                <Route exact path="/login" component={Login} />
+                <Route exact path="/add-item" component={NewListing} />
+                <Route exact path="/seller-signup" component={NewSeller} />
+                <Route exact path="/seller-home" component={SellerHome} />
+              </Switch>
+            <Footer />
+          </Router>
+      );
+    
+  }
 }
-
-export default App;
