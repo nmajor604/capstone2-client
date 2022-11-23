ebee8561f819498f16f0d455837d35b65ba6921e
nmajor604 <81140580+nmajor604@users.noreply.github.com>
2022-11-22 20:55:39 -0800

    Updated NewSeller post functionality

--- src/Components/NewSeller/NewSeller.js
+++ src/Components/NewSeller/NewSeller.js
@@ -1,32 +1,66 @@
-import React from 'react';
+import axios from 'axios';
+import { useRef, useState, useEffect } from 'react';
 
 const NewSeller = () => {
+
+    const [seller, setSeller] = useState('');
+    const [pwd, setPwd] = useState('');
+    const [confPwd, setConfPwd] = useState('');
+
+    const handleSubmit = async (e) => {
+        console.log('User registered')
+        e.preventDefault();
+         axios
+         .post('http://localhost:5050/', {
+            seller: seller,
+            password: pwd
+         })
+         .then((res) => {
+            console.log(res);
+         })
+         .catch((err) => {
+            console.log(err);
+         });
+
+         e.target.reset();
+    }
+
         return(
-          <div>
+          <form onSubmit={handleSubmit}>
               <div>
-                  <label for="firstName">First Name </label>
-                  <input type="text" id="firstName" placeholder="First Name"/>
-              </div>
-              <div>
-                  <label for="lastName">Last Name </label>
-                  <input type="text" name="" id="lastName" placeholder="Last Name"/>
-              </div>
-              <div>
-                  <label for="email">Email </label>
-                  <input type="email" id="email" placeholder="Email"/>
+                  <label for="username">Username</label>
+                  <input 
+                    type="text" 
+                    id="username" 
+                    placeholder="Username"
+                    onChange={(e) => setSeller(e.target.value)}
+                    value={seller}
+                />
               </div>
               <div>
                   <label for="password">Password </label>
-                  <input type="password" id="password" placeholder="Password"/>
+                  <input 
+                    type="password" 
+                    id="password" 
+                    placeholder="Password"
+                    onChange={(e) => setPwd(e.target.value)}
+                    value={pwd}
+                />
               </div>
               <div>
                   <label for="confirmPassword">Confirm Password </label>
-                  <input type="password" id="confirmPassword" placeholder="Confirm Password"/>
+                  <input 
+                    type="password" 
+                    id="confirmPassword" 
+                    placeholder="Confirm Password"
+                    onChange={(e) => setConfPwd(e.target.value)}
+                    value={confPwd}
+                />
               </div>
               <div>
               <button type="submit">Start Selling!</button>
             </div>
-          </div>
+          </form>
         )
 }
 
