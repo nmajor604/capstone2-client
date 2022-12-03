ba1d99d734161223df684bf6d82ab26b3d705d73
nmajor604 <81140580+nmajor604@users.noreply.github.com>
2022-12-03 01:58:30 -0800

    Revamped styling of home page

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -7,33 +7,82 @@ import hero from '../../assets/sydmead.jpg';
 function Home() {
 
   return (
-    <div className='home__body'>
-      <div className='header__container'>
-        <div className='header__title--1'><h1>Cleaning out your closet?</h1></div>
-        <div className='header__title--2'><h3>Looking to complete your collection?</h3></div>
-        <div className='header__title--3'><h5>But most importantly...</h5></div>
-        <div className='header__title--4'><h2>Tired of playing email tag with sellers and buyers?</h2></div>
-        <div className='header__title--5'><h1>Let's CONNEX!</h1></div>
-        <div className='divlink'>
-          <Link to="/listings">
-            <h1>Buy Something</h1>
-          </Link>
-        </div>
-        <div className='divlink'>
-          <Link to="/seller-signup">
-              <h1>Sell Something</h1>
-          </Link>
-        </div>
-      </div>
-      <div>
-        <img className='hero__image'
-          src={hero}
-            alt='Syd Mead Painting'>
-              
-        </img>
-      </div>
+    <>
+      <div class="hero_area">
     
-    </div>
+    <header class="header_section">
+      <div class="container">
+        <nav class="navbar navbar-expand-lg custom_nav-container">
+          <a class="navbar-brand" href="index.html">
+            
+              Connex&trade;
+            
+          </a>
+            <div class="btn-container ml- ml-lg-4 d-flex justify-content-right">
+              <a>
+                Login
+              </a>
+              <a>
+                Sign Up
+              </a>
+            </div>
+        </nav>
+      </div>
+    </header>
+    
+    <section class="slider_section position-relative">
+        <div class="container">
+          <div class="row">
+            <div class="col-md-6">
+              <div class="slider_item-detail">
+                <div>
+                  <h1>
+                    Buy and Sell. <br />
+                    With Just One Click.
+                  </h1>
+                  <p>
+                    Cleaning out your closet? Filling up your closet? 
+                    Tired of playing endless email tag with buyers and seller? <br />
+                    Let's
+                    <span class="navbar-brand">
+                      Connex!
+                    </span>
+                    
+                  </p>
+                  <div class="d-flex">
+                    <a href="" class="text-uppercase shop-btn mr-2">
+                      Buy Something!
+                    </a>
+                    <a href="" class="text-uppercase custom_dark-btn">
+                      Sell Something!
+                    </a>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="col-md-6">
+              <div class="slider_img-box">
+                <div>
+                  <img src={hero} alt="" class="" />
+                </div>
+              </div>
+            </div>
+            </div>
+        </div>
+    </section>
+
+    
+  </div>
+  
+  <section class="container-fluid footer_section">
+    <p>
+      Copyright &copy; 2019 All Rights Reserved By
+      <a href="https://html.design/">Free Html Templates</a>
+    </p>
+  </section>
+  
+    </>
+    
   )
   
   
