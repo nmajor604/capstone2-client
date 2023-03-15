ed2b171de1d1dd1520f512641c868fe8fb614c8f
nmajor604 <81140580+nmajor604@users.noreply.github.com>
2023-03-14 20:37:05 -0700

    Moved product details to separate ListingDetail component

--- src/Components/Listings/Listings.js
+++ src/Components/Listings/Listings.js
@@ -49,30 +49,10 @@ class Listings extends React.Component {
                 <div className="layout_padding2">
                 <div className="card-deck">
                     {this.state.currentListings.map((item) => (
-                    <>
-                      <div className="card">
-                          <img
-                          className="card-img-top"
-                          src={item.image} 
-                          alt="description"
-                          
-                           />
-                        <div className="card-body">
-                          <div key={item.id}>
-                            <h5 className="card-title">{item.item_name}</h5>
-                          </div>
-                          <p className="card-text">{item.item_description}</p>
-                          <div className="card-text">${item.price}</div>
-                          <div className="card-text">{item.item_neighbourhood}</div>
-                            <button onClick={(e) => this.buyItem(item.id, e)} className="btn btn-primary">
-                              BUY THIS ITEM
-                            </button>
-                            {/* <button onClick={(e) => this.deleteItem(item.id, e)} className="custom_orange-btn">
-                              <p>DELETE THIS ITEM</p>
-                            </button> */}
-                        </div>
-                      </div>
-                    </>
+                      <ProductItem
+                      
+                    />
+                    
                   ))}
                   
                   </div>
